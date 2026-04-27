d54f397192339cf89b5144bd115bef3b763dd977
chocoboMate <mmtmeriel.coding@gmail.com>
2026-04-27 15:38:09 +0800

    Remove practiceLessons from portfolio repository

--- cypress/e2e/practiceLessons/lesson1.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Practice Page Test', () => {
-  it('should visit page and find text', () => {
-    cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
-    cy.contains('h1', 'Practice Page').should('be.visible')
-  })
-})
